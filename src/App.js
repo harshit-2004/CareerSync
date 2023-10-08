fc556eb78363f4a8af08cf78c350842204215bb8
Harshit <harshit.it.21@nitj.ac.in>
2023-10-08 15:46:31 +0530

    commited code befor github upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Linemake from './first_page/Linemake.js';
 
 function App() {
   return (
-    <h1>say hello</h1>
+    <div className='body'>
+      <div className='loginclass'>
+        <button id='loginbutton' src="">LOGIN</button>
+      </div>
+      <div className='logoclass1'>
+        <img id='logoelement1' src="/careersynclogo.svg" alt="CareerSync Logo" />
+      </div>
+      <Linemake thickness='90'/>
+      <div className='gif-and-text'>
+        <img src='/staffing.gif'></img>
+        <div className='text-1024'>
+          <div id='text-1'>
+            <b>Crafting</b> <u>Careers</u>
+          </div>
+          <div id='text-1'>
+            <b>Connecting</b> <u>Futures</u>
+          </div>
+        </div>
+      </div>
+      <Linemake thickness='95'/>
+      <div className='line2'>
+        <div id='line2making'/>
+      </div>
+      <div className='line2'>
+        <div id='line3making'/>
+      </div>
+
+      <Linemake thickness='80'/>
+      <div className='bigbox'>
+        <div className='text2'>
+          <div id='text2-1'>
+            Keep track of All Your
+          </div>
+          <div id='text2-2'>
+            Application
+          </div>
+          <div id='text2-3'>
+            At One Place
+          </div>
+        </div>
+        <img id='setimage3' src='/202101-Talent-Trends-1_bi4qgd.gif'></img>
+      </div>
+
+      <Linemake thickness='80'/>
+      <div className='line2'>
+        <div id='line4making'/>
+      </div>
+      <Linemake thickness='95'/>
+
+      <div>
+        
+      </div>
+    </div>
   );
 }
 
---
+++ src/first_page/Linemake.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Linemake.css';
+
+export default function Linemake(props) {
+    var thickness = props.thickness;
+    return (
+        <div className='line-container' >
+            <div className='line-making' style={{ width: `${thickness}%` }}/>
+            <div className='line-making'/>
+        </div>
+    );
+};
