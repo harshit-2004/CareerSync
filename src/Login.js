d32bf17697250efb6d5f502f3659dabcc2a12d5d
Khalid Meraj <124028350+Khaliditsme@users.noreply.github.com>
2023-10-11 22:25:45 +0530

    k1 first commit

---
+++ src/Login.js
@@ -0,0 +1,21 @@
+
+
+import React from 'react'
+import './Login.css'
+function Login() {
+  return (
+      <div>
+           <div>
+               <img id='logo' src="/careersynclogo.svg" alt="CareerSync Logo" />
+               <h1>CAREER SYNC</h1>
+           </div>
+
+          <div>
+               <img src="/ezgif.gif" id='box'></img>
+
+          </div>
+      </div>
+  );
+}
+
+export default Login;
