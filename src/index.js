f8655f1aa0d0fcf27fc715daaffb8955f22f795d
Harshit <harshit.it.21@nitj.ac.in>
2023-10-13 02:16:18 +0530

    done Student portal page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import StudentPortal from './StudentPortal.js';
 import App from './App.js'
 import Login from './Login.js';
 
@@ -9,5 +10,6 @@ root.render(
   <React.StrictMode>
     {/* <Login/> */}
     <App />
+    {/* <StudentPortal /> */}
   </React.StrictMode>
 );
